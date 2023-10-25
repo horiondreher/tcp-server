b7c4418827fcdc1b8095edd9e7c20de86d05da2d
horiondreher <horiondreher@gmail.com>
2023-10-24 23:45:45 -0300

    Adding config file variables

--- src/server.cpp
+++ src/server.cpp
@@ -9,10 +9,57 @@
 #include <boost/bind/bind.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/enable_shared_from_this.hpp>
+
 #include <boost/regex.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 using namespace boost::asio;
 using ip::tcp;
+
+static const std::string config_file = "config.json";
+
+/**
+ * Singleton class to load config files and read without instanciating everytime
+*/
+class config {
+public:
+    int port = 0;
+    int max_file_size = 0;
+    std::string file_name_prefix = "";
+
+    static config& get_instance() {
+        static config instance;
+        return instance;
+    }
+
+    bool load_config(const std::string& config_file) {
+        try {
+            boost::property_tree::read_json(config_file, config_);
+            port = get_config_value<int>("server_port");
+            max_file_size = get_config_value<int>("max_file_size");
+            file_name_prefix = get_config_value<std::string>("file_name_prefix");
+
+            return true;
+        } catch (const boost::property_tree::ptree_error& e) {
+            std::cerr << "Error reading config file: " << e.what() << std::endl;
+            return false;
+        }
+    }
+
+private:
+    boost::property_tree::ptree config_;
+
+    config() {}
+
+    config(const config&) = delete;
+    config& operator=(const config&) = delete;
+    
+    template <typename T>
+    T get_config_value(const std::string& key) const {
+        return config_.get<T>(key);
+    }
+};
 
 class tcp_connection : public boost::enable_shared_from_this<tcp_connection> {
 private:
@@ -20,10 +67,8 @@ private:
     deadline_timer timer_;
 
     streambuf buffer_;
-    std::string message_;
     std::string file_name_;
     std::string client_endpoint_;
-    size_t max_file_length_ = 32;
 
     enum { max_length = 1024 };
     char data_[max_length];
@@ -57,7 +102,8 @@ public:
         client_endpoint_ = socket_.remote_endpoint().address().to_string() +
             ":" + std::to_string(socket_.remote_endpoint().port());
 
-        file_name_ = "CONNECTION_" + client_endpoint_ + "_" + timestamp + ".txt";
+        std::string prefix = config::get_instance().file_name_prefix;
+        file_name_ = prefix + "_" + client_endpoint_ + "_" + timestamp + ".txt";
 
         async_read_until(
             socket_,
@@ -81,19 +127,20 @@ public:
 
             std::cout << "Bytes received from " << client_endpoint_ << ": " << bytes_transferred << std::endl;
             size_t file_length = 0;
+            size_t max_file_size = config::get_instance().max_file_size;
 
             while(input_stream) {
                 input_stream.read(data_, max_length);
                 std::streamsize read = input_stream.gcount();
 
-                if(file_length + read > max_file_length_) {
-                    read = max_file_length_ - file_length;
+                if(file_length + read > max_file_size) {
+                    read = max_file_size - file_length;
                 }
 
                 output_file.write(data_, read);
                 file_length += read;
 
-                if(file_length >= max_file_length_) {
+                if(file_length >= max_file_size) {
                     break;
                 }
             }
@@ -129,7 +176,10 @@ private:
     }
 
 public:
-    tcp_server(io_context& io_context): io_context_(io_context), acceptor_(io_context, tcp::endpoint(tcp::v4(), 8080)) {
+    tcp_server(io_context& io_context, ip::port_type port) : 
+        io_context_(io_context), 
+        acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) 
+    {
         start_accept();
     }
 
@@ -147,11 +197,19 @@ public:
  * socket creates using the acceptor object
 */
 int main() {
-    std::cout << "Server started" << std::endl;
+    std::cout << "Read config file" << std::endl;
+
+    config& config = config::get_instance();
+    if(!config.load_config(config_file)) {
+        std::cerr << "Error loading config file, closing server" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Starting server on port: " << config.port << std::endl;
 
     try {
         io_context io_context;
-        tcp_server server(io_context);
+        tcp_server server(io_context, config.port);
 
         io_context.run();
     } catch (std::exception& e) {
